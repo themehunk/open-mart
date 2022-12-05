619904bc6926210df578753cc691151a594b7836
iheeresh <heereshd@gmail.com>
2022-12-05 17:28:17 +0530

    theme tested with WordPress 6.1

--- inc/woocommerce/woo-function.php
+++ inc/woocommerce/woo-function.php
@@ -183,6 +183,8 @@ remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_l
 remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open');
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
 remove_action('woocommerce_init','th_compare_add_action_shop_list');
+//To disable th compare Pro button 
+remove_action('woocommerce_init', 'tpcp_add_action_shop_list');
 
 //To integrate with a theme, please use bellow filters to hide the default buttons. hide default wishlist button on product archive page
 add_filter( 'woosw_button_position_archive', function() {
@@ -238,7 +240,7 @@ if ( ! function_exists('open_mart_whish_list_both')){
 
 if ( ! function_exists('open_mart_add_to_compare_fltr_both')){
 function open_mart_add_to_compare_fltr_both($pid){
-  if( ( class_exists( 'th_product_compare' ))){
+  if( class_exists( 'th_product_compare' ) || class_exists('Tpcp_product_compare') ){
              open_mart_add_to_compare_fltr($pid);      
                 }
                   elseif( ( class_exists( 'WPCleverWoosc' ))){
@@ -256,11 +258,11 @@ function open_mart_add_to_compare_fltr_both($pid){
 /************************/
 
 function open_mart_add_to_compare_fltr_single($pid){
-        if(class_exists(('th_product_compare') )){
+        if(class_exists('th_product_compare') || class_exists('Tpcp_product_compare')){
     global $product;
     $pid = $product->get_id();
     echo '<div class="thunk-compare"><span class="compare-list"><div class="woocommerce product compare-button">
-          <a class="th-product-compare-btn compare" data-th-product-id="'.$pid.'"></a>
+          <a class="th-product-compare-btn compare" data-th-product-id="'.esc_attr($pid).'"></a>
           </div></span></div>';
 
            }
@@ -269,11 +271,11 @@ function open_mart_add_to_compare_fltr_single($pid){
 
 
 function open_mart_add_to_compare_fltr($pid){
-      if(class_exists(('th_product_compare') )){
+      if(class_exists('th_product_compare') || class_exists('Tpcp_product_compare')){
     global $product;
     $pid = $product->get_id();
     echo '<div class="thunk-compare"><span class="compare-list"><div class="woocommerce product compare-button">
-          <a class="th-product-compare-btn compare" data-th-product-id="'.$pid.'"></a>
+          <a class="th-product-compare-btn compare" data-th-product-id="'.esc_attr($pid).'"></a>
           </div></span></div>';
 
            }elseif( ( class_exists( 'WPCleverWoosc' ))){
